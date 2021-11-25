f656d24bd23a92ae3a2eb1d32d6c95a7d982ce74
ANTHONY GABRIEL ALONSO GOMEZ <e1314620855@live.uleam.edu.ec>
2021-11-25 16:50:49 -0500

    Agregar archivos de proyecto.

---
+++ Herencia/Profesores.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herencia
+{
+    class Profesor : Empleado
+    {
+        public string Departamento { get; set; }
+        public Profesor(string Nombre, string Apellido, int Cedula, string EstadoCivil, int NumeroDespacho, int AñoIncorporacion, string Departamento) : base(Nombre, Apellido, Cedula, EstadoCivil, NumeroDespacho, AñoIncorporacion)
+        {
+            this.Departamento = Departamento;
+        }
+        public void MostrarProfesor()
+        {
+            Console.WriteLine("-----------------\nPROFESOR\n-----------------");
+            Console.WriteLine("Nombre: " + Nombre + "\nApellido: " + Apellido);
+            Console.WriteLine("Cedula: " + Cedula);
+            Console.WriteLine("Estado Civil: " + EstadoCivil);
+            Console.WriteLine("Despacho: " + NumeroDespacho);
+            Console.WriteLine("Año Incorporacion: " + AñoIncorporacion);
+            Console.WriteLine("Departamento: " + Departamento);
+        }
+        public void CambioDepartamento()
+        {
+            string NuevoDepartamento;
+            Console.WriteLine("Departamento al que quiera cambiar");
+            NuevoDepartamento = Convert.ToString(Console.ReadLine());
+            Departamento = NuevoDepartamento;
+            Console.WriteLine("Departamento nuevo: " + Departamento);
+        }
+        public void MostrarNewProfesor()
+        {
+            Console.WriteLine("-----------------\nPROFESOR\n-----------------");
+            Console.WriteLine("Nombre: " + Nombre + "\nApellido: " + Apellido);
+            Console.WriteLine("Cedula: " + Cedula);
+            Console.WriteLine("Estado Civil: " + EstadoCivil);
+            Console.WriteLine("Despacho: " + NumeroDespacho);
+            Console.WriteLine("Año Incorporacion: " + AñoIncorporacion);
+            Console.WriteLine("Departamento: " + Departamento);
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante Fabricio = new Estudiante("Fabricio", "Chavez", 1314620855, "Soltero", 8);
+            Fabricio.MostrarEstudiante();
+            Fabricio.CambioEstadoCivil();
+            Fabricio.CambiarCurso();
+
+            Profesor Enrique = new Profesor("Enrique", "Segoviano", 1308328569, "Viudo", 34, 2006, "Matemáticas");
+            Enrique.MostrarProfesor();
+            Enrique.CambioEstadoCivil();
+            Enrique.CambioDespacho();
+            Enrique.CambioDepartamento();
+
+            Personal Eddu = new Personal("Eddu", "Copiano", 1324571852, "Casado", 10, 2002, "Patio");
+            Eddu.MostrarEmpleado();
+            Eddu.CambioEstadoCivil();
+            Eddu.CambioDespacho();
+            Eddu.CambioSeccion();
+
+            Fabricio.MostrarNewEstudiante();
+            Enrique.MostrarNewProfesor();
+            Eddu.MostrarNewEmpleado();
+        }
+    }
+}
